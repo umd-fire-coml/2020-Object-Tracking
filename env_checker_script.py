4dbdfd5278403357a9979c6e13ddb2d537031bc5
Jacob Ginzburg <jginz1@terpmail.umd.edu>
2020-09-14 22:51:46 -0400

    checker script added

---
+++ env_checker_script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import tensorflow as tf
+import numpy as np
+import matplotlib as plt
+import cv2
+
+
+# In[13]:
+
+
+print(tf.__version__);
+print(np.__version__);
+print(plt.__version__);
+print(cv2.__version__);
+
+
+# In[ ]:
+
+
+
+
