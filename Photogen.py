2df45c58e503d1e63cd5ca4a6e8a56fc673c7336
mdiaby1 <64927434+mdiaby1@users.noreply.github.com>
2020-09-17 23:06:16 -0400

    Add files via upload

---
+++ Photogen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import os
+
+
+# In[ ]:
+
+
+image = plt.imread('<Insert photo filename here>')
+plt.imshow(image)
+plt.show()
+
