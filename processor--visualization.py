516d42c16a9ea727565841b9c031df4c0fc00c8e
Joyce Tijani <joytijani01@gmail.com>
2020-10-20 15:07:58 -0400

    Updated Version of script checking data generator

--- processor--visualization.py
+++ processor--visualization.py
@@ -1,12 +1,26 @@
 import numpy as np
+import time
 from keras.models import Sequential
-from my_classes import DataGenerator
+from data_generator import DataGenerator
 
+annotation_file='data/annotations/annotations/instances_val2017.json'
+list_IDs, labels = parse_data(annotation_file)
 imagecount = 0
-generator = DataGenerator( list_IDS, labels)
+# ignore self, 
+generator = DataGenerator(list_IDS, labels)
 
+# call time it, start time here
+start = time.time() 
 for x in range(100):
     imgs,label = next(generator)
-    imagecount += 1
+    imagecount += imgs.size[0]
 
+# record end time
+end = time.time()
+# then calculate it
+time = end - start
+print('Time per batch: ' + time/imagecount)
+print('Time per second: ' + imagecount/time)
+plt.imshow(img)
+plt.show()
 print(imagecount)
