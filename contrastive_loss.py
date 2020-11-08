c3bb0262ccc14c22bbf56f3870d72ed7cbf5aa3b
Jacob Ginzburg <jginz1@terpmail.umd.edu>
2020-11-08 15:09:15 -0500

    fixed typo

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -5,6 +5,6 @@ import tensorflow as tf
 
 def contrastive_loss(model1, model2, label, margin=2.0):
     distance = tf.sqrt(tf.reduce_sum(tf.pow(model1 - model2, 2), 1, keepdims=True))
-    similarity = y * tf.square(distance)
-    dissimilarity = (1 - y) * tf.square(tf.maximum((margin-distance), 0))
+    similarity = label * tf.square(distance)
+    dissimilarity = (1 - label) * tf.square(tf.maximum((margin-distance), 0))
     return tf.reduce_mean(dissimilarity + similarity) / 2
